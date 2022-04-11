82817d36385542a58e4d5b0940adff0135321429
Cristian Cázares <cm_cjavier@hotmail.com>
2022-04-11 14:29:23 -0600

    [ENHANCEMENT] Edited the 'show lyrics' interaction

--- Lyrics API/main.js
+++ Lyrics API/main.js
@@ -1,5 +1,6 @@
 let songs = [];
 let iterator = 0;
+const lyricsSpace = document.getElementById("lyrics");
 function Song(title, artist, lyrics){
 	this.title = title;
 	this.artist = artist;
@@ -7,11 +8,11 @@ function Song(title, artist, lyrics){
 }
 
 function main(){
-	findLyricsP("6 seconds of summer", "Teeth");
-	/*findLyricsP("Twenty One Pilots", "Lane Boy");
+	findLyricsP("5 seconds of summer", "Teeth");
+	findLyricsP("Twenty One Pilots", "Lane Boy");
 	findLyricsP("Imagine Dragons", "Enemy");
 	findLyricsP("5 Seconds of Summer", "She Looks So Perfect");
-	findLyricsP("VALORANT", "Die For You");*/
+	findLyricsP("VALORANT", "Die For You");
 }
 
 main();
@@ -21,25 +22,11 @@ main();
 
 
 
-function findLyrics(){
+function addSong(){
 	findLyricsP(document.getElementById("artist").value, document.getElementById("title").value);
-	/*console.log(
-		"https://api.lyrics.ovh/v1/" + 
-		document.getElementById("artist").value + "/" +
-		document.getElementById("title").value);
-	$.get(
-		"https://api.lyrics.ovh/v1/" + 
-		document.getElementById("artist").value + "/" +
-		document.getElementById("title").value,
-		function(data){
-			document.getElementById("lyrics").innerHTML=data.lyrics.replace(new RegExp("\n","g"), 
-			"<br>");
-		}
-	)*/
 }
 
 function findLyricsP(artist, title){
-	//console.log("https://api.lyrics.ovh/v1/" + artist + "/" + title);
 	$.ajax({
 		async: false,
 		type: 'GET',
@@ -56,20 +43,11 @@ function findLyricsP(artist, title){
 	});
 }
 
-function updateSongListASH2(song, index){
-	var h2 = document.createElement("h2");
-
-	h2.setAttribute("class", "song");
-	h2.setAttribute("onclick", "showLyrics(" + index + ")")
-	
-	h2.innerHTML = song.title + "/" + song.artist;
-	document.getElementById("songList").appendChild(h2);
-}
-
 function updateSongList(song, index){
 	var entry = document.createElement("tr");
 	entry.setAttribute("class", "song");
-	entry.setAttribute("onclick", "showLyrics(" + index + ")")
+	entry.setAttribute("onclick", "showLyrics(" + index + ", this)");
+	entry.setAttribute("data-state", "hidden");
 	
 	var cellArtist = document.createElement("td");
 	cellArtist.innerHTML = song.artist;
@@ -81,24 +59,48 @@ function updateSongList(song, index){
 	document.getElementById("songList").appendChild(entry);
 }
 
-function showLyrics(index){
-	document.getElementById("lyrics").innerHTML = songs[index].lyrics.replace(new RegExp("\n","g"), "<br>");
+function showLyrics(index, entry){
+	//If the clicked one is hidden
+	if(entry.dataset.state == "hidden"){
+		console.log("Hide all of them");
+		//Hide all of them
+		var songsEntries = getSongs();
+		songsEntries.forEach(song => {
+			song.dataset.state = "hidden";
+		});
+		console.log("Show it");
+		//Show the clicked one
+		lyricsSpace.innerHTML = songs[index].lyrics.replace(new RegExp("\n","g"), "<br>");
+		entry.dataset.state = "showed";
+	}else{
+		//If the clicked one is showed. Hide it
+		lyricsSpace.innerHTML = "";
+		entry.dataset.state = "hidden";
+	}
 }
 
-function deleteEntry(){
-	
+function deleteEntries(button){
+	if(button.dataset.state == "delete"){
+		button.innerHTML = "Done";
+		button.dataset.state = "done";
+
+		var songsEntries = getSongs();
+		songsEntries.forEach(song =>{
+			song.classList.add("deletable");
+			song.setAttribute("onclick", "deleteEntry(this)")
+		})
+	}else{
+		button.innerHTML = "Delete";
+		button.dataset.state = "delete";
+	}
 }
 
-/*
-function findLyricsP(id){
-	//console.log("https://api.lyrics.ovh/v1/" + artist + "/" + title);
-	$.ajax({
-		async: false,
-		type: 'GET',
-		url: "https://api.spotify.com/v1/playlists/" + id + "/tracks",
-		success: function(data){
-			console.log(data)
-		}
-	});
+function deleteEntry(entry){
+	entry.remove();
 }
-*/
+
+
+
+function getSongs(){
+	return document.querySelectorAll("[class=song]");
+}
