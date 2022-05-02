03004961a354539b976cb361c5fa5ff5ef27d034
Cristian Cázares <cm_cjavier@hotmail.com>
2022-05-02 11:24:28 -0600

    [ENHANCEMENT] Added Material UI and applied some basic styling and frontend routes

--- PERNPractice/client/src/App.js
+++ PERNPractice/client/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
-
+import Menu from "./components/Navbar";
+import {Container} from "@mui/material"
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<TaskList />} />
-        <Route path='/task/new' element={<TaskForm/>}/>
-        
-      </Routes>
+    <Menu></Menu>
+      <Container>
+        <Routes>
+          <Route path='/' element={<TaskList />} />
+          <Route path='/tasks/new' element={<TaskForm/>}/>
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
